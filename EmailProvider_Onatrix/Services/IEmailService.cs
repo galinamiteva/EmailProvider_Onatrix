e5bea19baec90f03ce5cceadfb820420b4411b86
galinamiteva <galina.miteva@utb.ecutbildning.se>
2024-10-04 20:28:30 +0200

    email sender redo allt funkar

---
+++ EmailProvider_Onatrix/Services/IEmailService.cs
@@ -0,0 +1,16 @@
+﻿using Azure.Messaging.ServiceBus;
+using EmailProvider_Onatrix.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailProvider_Onatrix.Services;
+
+public interface IEmailService
+{
+    EmailRequest UnpackEmailRequest(ServiceBusReceivedMessage message);
+
+    bool SendEmail(EmailRequest emailRequest);
+}
